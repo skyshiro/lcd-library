0ff0568ced1dfa77c64fedb043bb73e3193a1ce5
skyshiro <hodge.tannis@gmail.com>
2014-04-15 13:40:57 -0700

    Library is working!

--- lcd_driver.c
+++ lcd_driver.c
@@ -10,83 +10,150 @@ void lcd_setup()
 
 	//send set function 1
 	P2OUT = BIT2; //sets enable pin high
-	P1OUT = 0x2;  //MSB of function set
+	P1OUT |= 0x2;  //MSB of function set
 	P2OUT = 0x00; //turn off enable so data is latched in
-	P1OUT = 0x00;
+	P1OUT &= ~0x2;
 	//send set function 2
 	P2OUT = BIT2; //sets enable pin high
-	P1OUT = 0x8;  //LSB of function set
+	P1OUT |= 0x8;  //LSB of function set
 	P2OUT = 0x00; //turn off enable so data is latched in
-	P1OUT = 0x00;
+	P1OUT &= ~0x8;
 
 	__delay_cycles(40);
 
 	//Send display set 1
 	P2OUT = BIT2;
-	P1OUT = 0x0;
+	P1OUT |= 0x0;
 	P2OUT = 0x00; //turn off enable so data is latched in
-	P1OUT = 0x00;
+	P1OUT &= ~0x00;
 	//Send display set 2
 	P2OUT = BIT2;
-	P1OUT = 0xD;
+	P1OUT |= 0xD;
 	P2OUT = 0x00; //turn off enable so data is latched in
-	P1OUT = 0x00;
+	P1OUT &= ~0xD;
 
 	__delay_cycles(40);
 
 	//Send clear display 1
 	P2OUT = BIT2;
-	P1OUT = 0x0;
+	P1OUT |= 0x0;
 	P2OUT = 0x00; //turn off enable so data is latched in
-	P1OUT = 0x00;
+	P1OUT &= ~0x00;
 	//Send clear display 2
 	P2OUT = BIT2;
-	P1OUT = 0x1;
+	P1OUT |= 0x1;
 	P2OUT = 0x00; //turn off enable so data is latched in
-	P1OUT = 0x00;
+	P1OUT &= ~0x1;
 
 	__delay_cycles(1600);
 }
 
 void lcd_write_message(char *message)
 {
-	unsigned int i;
+	unsigned int i,j;
 
-	for(i=0;i<(16-strlen(message))/2;i++)
-		{
-			P2OUT = BIT0+BIT2;
-			P1OUT = (int)(' ')>>4;
-			P2OUT = BIT0; //turn off enable so data is latched in
-			P1OUT = 0x00;
-			P2OUT = 0x00;
+	//Send clear display 1
+	P2OUT = BIT2;
+	P1OUT |= 0x0;
+	P2OUT = 0x00; //turn off enable so data is latched in
+	P1OUT &= ~0x00;
+	//Send clear display 2
+	P2OUT = BIT2;
+	P1OUT |= 0x1;
+	P2OUT = 0x00; //turn off enable so data is latched in
+	P1OUT &= ~0x1;
 
-			P2OUT = BIT0+BIT2;
-			P1OUT = (int)(' ') & 0xF;
-			P2OUT = BIT0; //turn off enable so data is latched in
-			P1OUT = 0x00;
-			P2OUT = 0x00;
+	__delay_cycles(1600);
 
-			__delay_cycles(200);
+	if(strlen(message)<16)
+	{
 
-		}
+		for(i=0;i<(16-strlen(message))/2;i++)
+			{
+				P2OUT = BIT0+BIT2;
+				P1OUT |= (int)(' ')>>4;
+				P2OUT = BIT0; //turn off enable so data is latched in
+				P1OUT &= ~((int)(' ')>>4);
+				P2OUT = 0x00;
 
+				P2OUT = BIT0+BIT2;
+				P1OUT |= (int)(' ') & 0xF;
+				P2OUT = BIT0; //turn off enable so data is latched in
+				P1OUT &= ~((int)(' ') & 0xF);
+				P2OUT = 0x00;
+
+				__delay_cycles(200);
+
+			}
+
+			for(i=0;i<strlen(message);i++)
+			{
+				//change CGRAM data
+
+				P2OUT = BIT0+BIT2;
+				P1OUT |= (int)message[i]>>4;
+				P2OUT = BIT0; //turn off enable so data is latched in
+				P1OUT &= ~((int)message[i]>>4);
+				P2OUT = 0x00;
+
+				P2OUT = BIT0+BIT2;
+				P1OUT |= (int)message[i] & 0xF;
+				P2OUT = BIT0; //turn off enable so data is latched in
+				P1OUT &= ~((int)message[i] & 0xF);
+				P2OUT = 0x00;
+
+				__delay_cycles(200);
+			}
+	}
+	else
+	{
 		for(i=0;i<strlen(message);i++)
-		{
-			//change CGRAM data
+			{
+				//Change address back to bottom left 1
+				P2OUT = BIT2;
+				P1OUT = 0xC;
+				P2OUT = 0x00; //turn off enable so data is latched in
+				P1OUT = 0x00;
+				//Change address back to bottom left 2
+				P2OUT = BIT2;
+				P1OUT = 0x0;
+				P2OUT = 0x00; //turn off enable so data is latched in
+				P1OUT = 0x00;
 
-			P2OUT = BIT0+BIT2;
-			P1OUT = (int)message[i]>>4;
-			P2OUT = BIT0; //turn off enable so data is latched in
-			P1OUT = 0x00;
-			P2OUT = 0x00;
+				__delay_cycles(200);
 
-			P2OUT = BIT0+BIT2;
-			P1OUT = (int)message[i] & 0xF;
-			P2OUT = BIT0; //turn off enable so data is latched in
-			P1OUT = 0x00;
-			P2OUT = 0x00;
+				for (j = 0; j < 16; j++)
+				{
+					//change CGRAM data
 
-			__delay_cycles(200);
-		}
+					P2OUT = BIT0+BIT2;
+					P1OUT = (int)message[i+j]>>4;
+					P2OUT = BIT0; //turn off enable so data is latched in
+					P1OUT = 0x00;
+					P2OUT = 0x00;
 
+					P2OUT = BIT0+BIT2;
+					P1OUT = (int)message[i+j] & 0xF;
+					P2OUT = BIT0; //turn off enable so data is latched in
+					P1OUT = 0x00;
+					P2OUT = 0x00;
+
+					__delay_cycles(200);
+
+				}
+
+				//Change address back to bottom left 1
+				P2OUT = BIT2;
+				P1OUT = 0xC;
+				P2OUT = 0x00; //turn off enable so data is latched in
+				P1OUT = 0x00;
+				//Change address back to bottom left 2
+				P2OUT = BIT2;
+				P1OUT = 0x0;
+				P2OUT = 0x00; //turn off enable so data is latched in
+				P1OUT = 0x00;
+
+				__delay_cycles(50000);
+			}
+	}
 }
